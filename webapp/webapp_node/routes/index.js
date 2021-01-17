c034419b432b2181ce3da10b49c9dad4718b1441
kaorv <kaorvrvrvrv@gmail.com>
2021-01-17 20:52:01 +0900

    06_Docker

--- webapp/webapp_node/routes/index.js
+++ webapp/webapp_node/routes/index.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 
 var mysql = require('mysql');
 var mysql_setting = {
-  host     : 'localhost',
-  user     : 'root',
+  host     : 'mysql',
+  user     : 'reco',
   password : 'passwords',
   database : 'webapp'
 };
+
 var connection = mysql.createConnection(mysql_setting);
 
 router.get("/top",(req,res)=>{
@@ -14,21 +15,19 @@ router.get("/top",(req,res)=>{
 });
 
 router.get("/purchase",(req,res)=>{
-
   connection.query('SELECT * from itemdetails', (err, itemdetail) => {
     if (err) throw err;
     res.render("./purchase.ejs",{ itemdetail: itemdetail });
   });
-
 });
 
 router.get("/output",(req,res)=>{
-  let item = req.query.item;
+  var item = req.query.item;
 	console.log("output item-----------------------------");
 	console.log(item);
   console.log("----------------------------------------");
   
-  const additem = { item: item };
+  var additem = { item: item };
   connection.query('INSERT INTO purchasehistory SET ?', additem,(err) => {
     if (err) throw err;
   	console.log('itemをtableに追加しました。');
